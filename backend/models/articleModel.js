f9f2944f736c86b418d57efb289d4b3c67247276
rentorious <rentorious@gmail.com>
2021-04-04 09:50:45 +0200

    feature: Mongodb article model

---
+++ backend/models/articleModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const articleSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    content_serbian: { type: String, required: true },
+    content_finnish: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Article = mongoose.model("Article", articleSchema);
+export default Article;
